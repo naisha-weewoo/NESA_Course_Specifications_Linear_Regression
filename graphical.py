7ab8c60da7c8f6ce1bf5ed4b679adc238d060600
Ben Jones <benpaddlejones@outlook.com>
2024-12-05 23:03:52 +1100

    add graphs

--- graphical.py
+++ graphical.py
@@ -1,5 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('deeplearning.mplstyle')
 from sklearn.linear_model import LinearRegression
+
+
 x = np.array([[2], [4], [6], [8], [10], [12], [14], [16]])
 y = np.array([1, 3, 5, 7, 9, 11, 13, 15])
 model = LinearRegression()
